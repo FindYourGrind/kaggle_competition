5ec8070e9cb299dab7fbe932be452e69aba81217
IT <igortram@gmail.com>
2017-08-11 08:41:00 +0300

    DataSciencePastime1

--- Titanic/treeTest.py
+++ Titanic/treeTest.py
@@ -12,8 +12,8 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import Imputer
 
-titanic_train_data = pd.read_csv(r"./data/train.csv")
-titanic_test_data = pd.read_csv(r"./data/test.csv")
+titanic_train_data = pd.read_csv(r"Titanic/data/train.csv")
+titanic_test_data = pd.read_csv(r"Titanic/data/test.csv")
 
 def preprocessing(data):
     processed_data = data
@@ -21,31 +21,31 @@ def preprocessing(data):
     def nan_padding(data, columns):
         for column in columns:
             imputer = Imputer()
-            data[column] = imputer.fit_transform(data[column].values.reshape(-1, 1))
+            data.loc[:, column] = imputer.fit_transform(data[column].values.reshape(-1, 1))
         return data
 
     processed_data = nan_padding(processed_data, ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare'])
 
-    processed_data['Title'] = processed_data.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    processed_data.loc[:, 'Title'] = processed_data.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
     pd.crosstab(processed_data['Title'], processed_data['Sex'])
-    processed_data['Title'] = processed_data['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
-    processed_data['Title'] = processed_data['Title'].replace('Mlle', 'Miss')
-    processed_data['Title'] = processed_data['Title'].replace('Ms', 'Miss')
-    processed_data['Title'] = processed_data['Title'].replace('Mme', 'Mrs')
+    processed_data.loc[:, 'Title'] = processed_data['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    processed_data.loc[:, 'Title'] = processed_data['Title'].replace('Mlle', 'Miss')
+    processed_data.loc[:, 'Title'] = processed_data['Title'].replace('Ms', 'Miss')
+    processed_data.loc[:, 'Title'] = processed_data['Title'].replace('Mme', 'Mrs')
     processed_data = processed_data.drop(['Name'], axis=1)
 
     title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-    processed_data['Title'] = processed_data['Title'].map(title_mapping)
-    processed_data['Title'] = processed_data['Title'].fillna(0)
+    processed_data.loc[:, 'Title'] = processed_data['Title'].map(title_mapping)
+    processed_data.loc[:, 'Title'] = processed_data['Title'].fillna(0)
 
-    processed_data['FamilySize'] = processed_data['SibSp'] + processed_data['Parch'] + 1
-    processed_data['IsAlone'] = 0
+    processed_data.loc[:, 'FamilySize'] = processed_data['SibSp'] + processed_data['Parch'] + 1
+    processed_data.loc[:, 'IsAlone'] = 0
     processed_data.loc[processed_data['FamilySize'] == 1, 'IsAlone'] = 1
     processed_data = processed_data.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
 
     freq_port = processed_data.Embarked.dropna().mode()[0]
-    processed_data['Embarked'] = processed_data['Embarked'].fillna(freq_port)
-    processed_data['Embarked'] = processed_data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+    processed_data.loc[:, 'Embarked'] = processed_data['Embarked'].fillna(freq_port)
+    processed_data.loc[:, 'Embarked'] = processed_data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
 
     def dummy_data(data, columns):
         for column in columns:
@@ -58,14 +58,14 @@ def preprocessing(data):
     def sex_to_int(data):
         le = LabelEncoder()
         le.fit(["male", "female"])
-        data["Sex"] = le.transform(data["Sex"])
+        data.loc[:, "Sex"] = le.transform(data["Sex"])
         return data
 
     processed_data = sex_to_int(processed_data)
 
     def normalize_age(data):
         scaler = MinMaxScaler()
-        data["Age"] = scaler.fit_transform(data["Age"].values.reshape(-1, 1))
+        data.loc[:, "Age"] = scaler.fit_transform(data["Age"].values.reshape(-1, 1))
         return data
 
     processed_data = normalize_age(processed_data)
@@ -73,7 +73,7 @@ def preprocessing(data):
     return processed_data
 
 
-def split_valid_test_data(data, fraction=(1 - 0.95)):
+def split_valid_test_data(data, fraction=(1 - 0.9)):
     data_y = data["Survived"]
     lb = LabelBinarizer()
     data_y = lb.fit_transform(data_y)
@@ -86,51 +86,68 @@ def split_valid_test_data(data, fraction=(1 - 0.95)):
 processed_train_data = titanic_train_data[['Survived', 'Name', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 input_x, input_y, output_x, output_y = split_valid_test_data(preprocessing(processed_train_data))
 
+processed_test_data = titanic_test_data[['Name', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+input_y = preprocessing(processed_test_data)
 
+output_x = output_x.reshape((output_x.size,));
+
+##
 titanic_tree_model = DecisionTreeRegressor()
 titanic_tree_model.fit(input_x, output_x)
 prediction = titanic_tree_model.predict(input_y)
 
-print('DecisionTreeRegressor accuracy: ', round(titanic_tree_model.score(input_y, output_y) * 100, 2))
-print('DecisionTreeRegressor error: ', mean_absolute_error(output_y, prediction))
-print('\n')
+# print('DecisionTreeRegressor accuracy: ', round(titanic_tree_model.score(input_y, output_y) * 100, 2))
+# print('DecisionTreeRegressor error: ', mean_absolute_error(output_y, prediction))
+# print('\n')
 
-
+##
 titanic_logreg_model = LogisticRegression()
 titanic_logreg_model.fit(input_x, output_x)
 prediction = titanic_logreg_model.predict(input_y)
 
-print('LogisticRegression accuracy: ', round(titanic_logreg_model.score(input_y, output_y) * 100, 2))
-print('LogisticRegression error: ', mean_absolute_error(output_y, prediction))
-print('\n')
+evaluation = titanic_test_data[['PassengerId']].copy()
+evaluation["Survived"] = prediction.astype(int)
+evaluation.to_csv("./Titanic/prediction/titanic_evaluation_submission_logistic_regression.csv", index=False)
 
+# print('LogisticRegression accuracy: ', round(titanic_logreg_model.score(input_y, output_y) * 100, 2))
+# print('LogisticRegression error: ', mean_absolute_error(output_y, prediction))
+# print('\n')
 
+##
 titanic_svc_model = SVC()
 titanic_svc_model.fit(input_x, output_x)
 prediction = titanic_svc_model.predict(input_y)
 
-print('SVC accuracy: ', round(titanic_svc_model.score(input_y, output_y) * 100, 2))
-print('SVC error: ', mean_absolute_error(output_y, prediction))
-print('\n')
+evaluation = titanic_test_data[['PassengerId']].copy()
+evaluation["Survived"] = prediction.astype(int)
+evaluation.to_csv("./Titanic/prediction/titanic_evaluation_submission_svc.csv", index=False)
 
+# print('SVC accuracy: ', round(titanic_svc_model.score(input_y, output_y) * 100, 2))
+# print('SVC error: ', mean_absolute_error(output_y, prediction))
+# print('\n')
 
+##
 titanic_knn_model = KNeighborsClassifier(n_neighbors=3)
 titanic_knn_model.fit(input_x, output_x)
 prediction = titanic_knn_model.predict(input_y)
 
-print('K-NN accuracy: ', round(titanic_knn_model.score(input_y, output_y) * 100, 2))
-print('K-NN error: ', mean_absolute_error(output_y, prediction))
-print('\n')
+evaluation = titanic_test_data[['PassengerId']].copy()
+evaluation["Survived"] = prediction.astype(int)
+evaluation.to_csv("./Titanic/prediction/titanic_evaluation_submission_knn.csv", index=False)
+
+# print('K-NN accuracy: ', round(titanic_knn_model.score(input_y, output_y) * 100, 2))
+# print('K-NN error: ', mean_absolute_error(output_y, prediction))
+# print('\n')
 
 
+##
 titanic_forestclass_model = RandomForestClassifier(n_estimators=100)
 titanic_forestclass_model.fit(input_x, output_x)
 prediction = titanic_forestclass_model.predict(input_y)
 
-print('RandomForestClassifier accuracy: ', round(titanic_forestclass_model.score(input_y, output_y) * 100, 2))
-print('RandomForestClassifier error: ', mean_absolute_error(output_y, prediction))
-print('\n')
-
+#print('RandomForestClassifier accuracy: ', round(titanic_forestclass_model.score(input_y, output_y) * 100, 2))
+#print('RandomForestClassifier error: ', mean_absolute_error(output_y, prediction))
+#print('\n')
 
 '''
 # Tree
